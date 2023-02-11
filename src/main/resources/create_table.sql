f111faaf5f2528c89f01684a1dad3276aa9587d7
= <yoko_myonai@yahoo.co.jp>
2023-02-11 17:07:36 +0900

    テーブルCREATE文を追加

---
+++ src/main/resources/create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `testtable` (
+  `id` int DEFAULT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `tel` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `helloworld` (
+  `helloworld` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
