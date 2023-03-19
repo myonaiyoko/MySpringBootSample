a81b65bfc931e8e2cb2da3bde2b9700e3a0a841a
myonaiyoko <myonaiyoko@gmail.com>
2023-03-19 15:50:16 +0000

    テーブル作成スクリプトにtodoテーブルを追加

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -3,7 +3,14 @@ CREATE TABLE `testtable` (
   `name` varchar(10) DEFAULT NULL,
   `tel` varchar(12) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `helloworld` (
   `helloworld` varchar(13) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `todo` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(20) DEFAULT NULL,
+  `memo` varchar(500) DEFAULT NULL,
+  `deadline` date DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
